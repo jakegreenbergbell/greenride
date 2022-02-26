30f0a0b2644bf95b19758bde0c0b4807300f38fd
BrysonXiao <40129652+BrysonXiao@users.noreply.github.com>
2022-02-26 15:02:32 -0800

    Fixed up the Car Info Form

--- app/src/App.js
+++ app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="w-screen h-screen flex flex-row">
       <div className="flex flex-col flex-[1] bg-blue-500 justify-center items-center">
-        Form
+        <CarInfoForm />
       </div>
       <div className="flex flex-[4] bg-slate-400 justify-center items-center w-full h-full">
         <Map />
