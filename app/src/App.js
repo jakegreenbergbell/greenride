3c78f9ae1a08356fc725460c72bc33dbebfd485c
jakegreenbergbell <32340594+jakegreenbergbell@users.noreply.github.com>
2022-02-26 11:07:26 -0800

    Created map component.

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import React from "react"
+import { GoogleMap, useLoadScript, Marker, withScriptjs, withGoogleMap, InfoWindow } from "@react-google-maps/api"
+import Map from './components/GoogleMap.js';
 
-function App() {
-  return (
-    <div className="App">
-      hi kyle and bryson
+const mapContainerStyle = {
+  width: "100%",
+  height: "100%",
+};
+
+const center = {
+  lat:  34.0689,
+  lng: -118.4451,
+};
+
+export default function App(){
+  return(
+    <div className="container">
+      <Map/>
     </div>
-  );
+  )
 }
-
-export default App;
--- app/src/components/GoogleMap.js
+++ app/src/components/GoogleMap.js
@@ -1,1 +1,31 @@
-import React from "react";
+import React from "react"
+import { GoogleMap, useLoadScript, Marker, withScriptjs, withGoogleMap, InfoWindow } from "@react-google-maps/api"
+
+const mapContainerStyle = {
+  width: "100%",
+  height: "100%",
+};
+
+const center = {
+  lat:  34.0689,
+  lng: -118.4451,
+};
+
+export default function Map(){
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: "AIzaSyAqhG7ACvDyaxkpvSSowGOgtfWuJqnZjzE"
+  })
+
+  if(loadError) return "Load error"
+  if(!isLoaded) return "Loading"
+  return(
+    <div className="container">
+      <GoogleMap 
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+      >
+      </GoogleMap>
+    </div>
+  )
+}
