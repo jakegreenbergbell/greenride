a78933bf453510bbc194c9d7b64c6110389d895e
BrysonXiao <40129652+BrysonXiao@users.noreply.github.com>
2022-02-26 10:46:23 -0800

    Added Tailwind container sizing for form section and map section

--- app/src/App.js
+++ app/src/App.js
@@ -3,9 +3,13 @@ import CarInfoForm from "./components/CarInfoForm";
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      hi kyle and bryson
-      <CarInfoForm />
+    <div className="w-screen h-screen flex flex-row">
+      <div className="flex flex-col flex-[1] bg-blue-500 justify-center items-center">
+        Form
+      </div>
+      <div className="flex flex-[4] bg-slate-400 justify-center items-center">
+        Map
+      </div>
     </div>
   );
 }
